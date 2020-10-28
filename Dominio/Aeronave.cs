ed9ea1a1c60867f3cba6a135a13cd05c01ace953
Fausto Reyes <fausto.reyes@est.uca.edu.ni>
2020-10-27 23:15:39 -0600

    Estructura de la aplicación + modelo de aeronave en la capa dominio y conexión a la base de datos

---
+++ Dominio/Aeronave.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    class Aeronave
+    {
+        public int aeronaveID { get; set; }
+        public String modelo { get; set; }
+    }
+}
