9b7b19569d9c36cd48c2d0e71a9cecd4474b8e8e
Jorge1Obando <jorge1obando@gmail.com>
2020-11-24 17:42:28 -0600

    chumete

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    class Usuario
+    {
+		public string Username { get; set; }
+
+		public string Nombre { get; set; }
+
+		public string Apellido { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Email { get; set; }
+
+		public string Pasaporte { get; set; }
+
+		public int paisID { get; set; }
+	}
+}
---
+++ Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace proyectoAPI.Models 
+{
+	public class Usuario
+	{
+		public string Username { get; set; }
+
+		public string Nombre { get; set; }
+
+		public string Apellido { get; set; }
+
+		public string Telefono { get; set; }
+
+		public string Email { get; set; }
+
+		public string Pasaporte { get; set; }
+
+		public int paisID { get; set; }
+		[ForeignKey("paisID")]
+
+	}
+
+}
+
