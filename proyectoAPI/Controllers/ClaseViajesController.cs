56eb7569d1e7657502365a2e3805950794ad6455
Jorge1Obando <jorge1obando@gmail.com>
2020-11-30 18:18:44 -0600

    shaaaaaaaaaaaaark

---
+++ proyectoAPI/Controllers/ClaseViajesController.cs
@@ -0,0 +1,36 @@
+﻿using Dominio;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.ClaseViaje;
+
+namespace proyectoAPI.Controllers
+{
+    [Route("api/[Controller]")]
+
+    public class ClaseViajesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ClaseViajesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ClaseViaje>>> Get()
+        {
+            return await _mediator.Send(new Consulta.ListaClaseViaje());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
+        {
+            return await _mediator.Send(data);
+        }
+    }
+}
---
+++ proyectoAPI/Controllers/VuelosController.cs
@@ -0,0 +1,20 @@
+﻿using Aplicacion.Butaca;
+using Dominio;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectoAPI.Controllers
+{
+
+    [Route("api/[Controller]")]
+
+    public class VuelosController : ControllerBase
+    {
+    }
+}
